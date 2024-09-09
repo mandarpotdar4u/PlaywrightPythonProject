89625f36be7a017269b71d16ad704cf1834f696d
Mandar Potdar <mandar.potdar@spoton.com>
2024-09-09 12:33:25 +0530

    1.Minor Update Correction

--- utilities/azuresqldbconnection.py
+++ utilities/azuresqldbconnection.py
@@ -6,6 +6,7 @@ database = 'QADB'
 username = 'admin01'
 password = 'admin@1234'
 driver = '{ODBC Driver 18 for SQL Server}'
+Encrypt = 'yes'
 
 # Create the connection
 connection_string = f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}'
